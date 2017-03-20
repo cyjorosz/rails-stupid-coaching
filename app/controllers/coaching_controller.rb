920183074395e3ebd8225ef33061823f083ba8cc
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2017-03-20 14:12:59 +0100

    Stupid coaching on rails

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,24 @@
+class CoachingController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    @coach_answer = coach_answer(@question)
+  end
+
+  def coach_answer(your_message)
+  if your_message.downcase == "i am going to work right now!"
+    ''
+  elsif your_message.end_with?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    "I don't care, get dressed and go to work!"
+  end
+  end
+
+end
+
+
